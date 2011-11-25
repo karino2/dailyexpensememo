1a426a919339a9f6bb7410dd2afc5404f65edcef
karino <karino@localhost>
2011-11-25 21:01:56 +0900

    Initial commit.

---
+++ DailyExpenseMemo/src/com/livejournal/karino2/DailyExpenseMemo/DailyExpenseMemoActivity.java
@@ -0,0 +1,13 @@
+package com.livejournal.karino2.DailyExpenseMemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DailyExpenseMemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
