c176054ad5e723fdba2489748b23794776d1db90
karino <karino@localhost>
2011-11-26 11:22:26 +0900

    Finish layout.

--- DailyExpenseMemo/src/com/livejournal/karino2/DailyExpenseMemo/DailyExpenseMemoActivity.java
+++ DailyExpenseMemo/src/com/livejournal/karino2/DailyExpenseMemo/DailyExpenseMemoActivity.java
@@ -1,13 +1,21 @@
 package com.livejournal.karino2.DailyExpenseMemo;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class DailyExpenseMemoActivity extends Activity {
+public class DailyExpenseMemoActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        final TabHost tabHost = getTabHost();
+
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("hoge")
+                .setContent(new Intent(this, EntryActivity.class)));
+        
     }
 }
---
+++ DailyExpenseMemo/src/com/livejournal/karino2/DailyExpenseMemo/EntryActivity.java
@@ -0,0 +1,13 @@
+package com.livejournal.karino2.DailyExpenseMemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EntryActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry);
+
+    }
+}
